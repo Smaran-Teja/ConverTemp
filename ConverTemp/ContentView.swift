1debcf365ea0b0c9979505e8f6cd65c6c2c6474f
Smaran-Teja-P <smaranteja32@gmail.com>
2024-05-15 16:27:25 -0400

    Basic layout and UI elements

--- ConverTemp/ContentView.swift
+++ ConverTemp/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tempScales = ["f", "c", "k"]
+    
+    @State private var selectedFrom = "c"
+    @State private var selectedTo = "c"
+    @State private var amt = 0.0
+    
+    @FocusState private var  fromUnitFocus: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section ("From") {
+                    Picker("Select unit", selection: $selectedFrom) {
+                        ForEach(tempScales, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                Section("Amount") {
+                    TextField("Enter amount", value: $amt, format: .number).keyboardType(.decimalPad)
+                        .focused($fromUnitFocus)
+                }
+                Section ("To") {
+                    Picker("Select unit", selection: $selectedTo) {
+                        ForEach(tempScales, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
