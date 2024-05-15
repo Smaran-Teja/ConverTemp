1da8ef0e234ae5207711fb45b34b734f62442316
Smaran-Teja-P <smaranteja32@gmail.com>
2024-05-15 16:04:15 -0400

    Initial Commit

---
+++ ConverTemp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConverTemp
+//
+//  Created by Smaran Teja on 15/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConverTemp/ConverTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverTempApp.swift
+//  ConverTemp
+//
+//  Created by Smaran Teja on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
